5418492287e79b2d6f03c4eed49ec228330ae884
thomas~ <mail@thms.cc>
2025-08-23 14:26:07 +0200

    Add maintenance mode and fix progress bar

--- ops/start-nginx.sh
+++ ops/start-nginx.sh
@@ -4,9 +4,11 @@ set -eu
 : "${ORS_API_KEY:?ORS_API_KEY not set}"
 : "${SEARCH_RADIUS_KM:=10}"
 : "${STEP_KM:=50}"
+: "${MAINTENANCE_MODE:=0}"
+: "${MAINTENANCE_KEY:=}"
 
 # shellcheck disable=SC2016
-envsubst '${ORS_API_KEY} ${SEARCH_RADIUS_KM} ${STEP_KM}' \
+envsubst '${ORS_API_KEY} ${SEARCH_RADIUS_KM} ${STEP_KM} ${MAINTENANCE_MODE} ${MAINTENANCE_KEY}' \
     < /usr/share/nginx/html/config.js.template \
     > /usr/share/nginx/html/config.js
 
